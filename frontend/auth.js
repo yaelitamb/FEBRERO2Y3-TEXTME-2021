3f86c89128974e47d9e12dfc62dcd4294037e224
yaelitamb <yaelmartin@uma.es>
2025-01-24 02:18:34 +0100

    Update auth.js

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,6 +1,17 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google],
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET, // Asegúrate de tener esta variable configurada
+  pages: {
+    signIn: "/signin", // Opcional, personaliza la página de inicio de sesión si es necesario
+    error: "/api/auth/error", // Página de error personalizada
+  },
+  debug: true, // Habilita depuración
+});
